4aea6fa3e0d23317b1ac1a444515b32a459645e5
kchiu <ntu.kchiu@gmail.com>
2011-03-08 00:44:20 +0800

    views: add login function to index
    Users have to enter their google accounts to login nutrobber, or we
    redirect them to google account registration page.

--- src/nutrobber/views.py
+++ src/nutrobber/views.py
@@ -1,4 +1,11 @@
+from google.appengine.api import users
+
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 def index(request):
-    return HttpResponse('Welcome to NutRobber!')
+    user = users.get_current_user()
+    if user:
+        return HttpResponse('Hello, ' + user.nickname())
+    else:
+        return HttpResponseRedirect(users.create_login_url(request.build_absolute_uri()))
