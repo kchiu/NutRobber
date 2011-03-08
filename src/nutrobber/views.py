eb235e5902ebffae4f1226bc9e69c6cc839c9afe
kchiu <ntu.kchiu@gmail.com>
2011-03-09 00:25:20 +0800

    environ: add js and template folders
    Add js and template folders and put some init files in them.
    Ex. html5 template, index template, modernizr.js

--- src/nutrobber/views.py
+++ src/nutrobber/views.py
@@ -2,10 +2,16 @@ from google.appengine.api import users
 
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.template import Context
+from django.template import loader
 
 def index(request):
     user = users.get_current_user()
+    signout = users.create_logout_url(request.build_absolute_uri())
     if user:
-        return HttpResponse('Hello, ' + user.nickname())
+        t = loader.get_template('index.html')
+        c = Context({'username':user.nickname(),
+                     'signout':signout})
+        return HttpResponse(t.render(c))
     else:
         return HttpResponseRedirect(users.create_login_url(request.build_absolute_uri()))
